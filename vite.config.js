21d2ad032a3e45e6692400859e3832be8b8c7cf3
Riskyo <octaviansusatyo@gmail.com>
2025-09-28 15:36:52 +0700

    konsol gaming

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', 
+        port: 5173,
+        hmr: {
+            host: '10.164.240.136', //ganti sesuai IP lokal
+        },
+    },
 });
